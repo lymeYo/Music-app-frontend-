a9e4b6458dcb6c3a356463843687dd1d0fcca5f1
lymeYo <lyme1878@mail.ru>
2023-05-01 16:15:48 +0300

    after binding back and front

--- src/ui/leaflet/MyLocationMarker.tsx
+++ src/ui/leaflet/MyLocationMarker.tsx
@@ -2,10 +2,7 @@ import React, { useState } from 'react'
 import { Marker, Popup, useMapEvents } from 'react-leaflet'
 import * as L from 'leaflet'
 
-const greenIcon = new L.Icon({
-  iconUrl:
-    'https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|2ecc71&chf=a,s,ee00FFFF'
-})
+import { greenIcon } from '@/ui/leaflet/icons'
 
 const MyLocationMarker = () => {
   const [position, setPosition] = useState<any>(null)
